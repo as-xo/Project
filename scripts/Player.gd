47ecad1b6bb9de3678f2ff0ae6fd6e3c00840432
as-xo <136635408+as-xo@users.noreply.github.com>
2024-07-29 16:42:27 +0100

    scripted player movement and dialogue

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,57 +1,65 @@
 extends CharacterBody2D
 
-const SPEED = 130.0
+const speed = 130.0
 const JUMP_VELOCITY = -300.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 @onready var animated_sprite = $AnimatedSprite2D
-#@onready var actionable_finder = $Direction/ActionableFinder
+var player_state
 
+func _physics_process(_delta):
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		
-#func _unhandled_input(_event: InputEvent) -> void:
-	#if Input.is_action_just_pressed("ui_accept"):
-		#var actionables = actionable_finder.get_overlapping_areas()
-		#if actionables.size() > 0:
-			#actionables[0].action()
-			#return
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("move_left", "move_right")
-	
-	#flip the sprite
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
-		
-	# animate sprite some more pls
-	# play animations
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
-		else:
-			animated_sprite.play("run")	
-	#else:
-		#animated_sprite.play("")	
-		# ADD TALK ANIMATION PLEASE
-	
-	# apply movement
-	if direction:
-		velocity.x = direction * SPEED
+	if direction.x == 0 and direction.y == 0:
+		player_state = "idle"
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		player_state = "run"	
+		
+	velocity = direction * speed		
+	move_and_slide()	
+	player_movement(direction)
+	
+func player_movement(dir):
+	if player_state == "idle":
+		animated_sprite.play("idle")
+	elif player_state == "run":
+		animated_sprite.play("run")
+		if dir.x < 0:
+			animated_sprite.flip_h = true
+		elif dir.x > 0:
+			animated_sprite.flip_h = false
+	
+	
+	#if Input.is_action_just_pressed("move_left"):
+		#velocity.x = -speed
+		#velocity.y = 0
+	#elif  Input.is_action_just_pressed("move_right"):
+		#velocity.x = speed
+		#velocity.y = 0
+	#elif Input.is_action_just_pressed("move_down"):
+		#velocity.y = speed
+		#velocity.x = 0		
+	#elif Input.is_action_just_pressed("move_up"):
+		#velocity.y = -speed
+		#velocity.x = 0
+	#elif Input.is_action_just_pressed("jump"):
+		#velocity.y = JUMP_VELOCITY
+	#else:
+		#velocity.x = 0
+		#velocity.y = 0	
+		#
+		#
+	#move_and_slide()	
+	
 
-	move_and_slide()
+##@onready var actionable_finder = $Direction/ActionableFinder
+
+##func _unhandled_input(_event: InputEvent) -> void:
+	##if Input.is_action_just_pressed("ui_accept"):
+		##var actionables = actionable_finder.get_overlapping_areas()
+		##if actionables.size() > 0:
+			##actionables[0].action()
+			##return
+#
+
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
