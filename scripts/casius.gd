e0b87aa6cf4418e4689c40f4121ccbae34134416
as-xo <136635408+as-xo@users.noreply.github.com>
2024-07-29 21:36:15 +0100

    added Casius npc and dialogue template

--- scripts/casius.gd
+++ scripts/casius.gd
@@ -1,13 +1,14 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
+
 #@onready var actionable_finder = $Direction/ActionableFinder
 
 var is_talking = false
 
 func _unhandled_input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("Nathan"):
-		DialogueManager.show_example_dialogue_balloon(load("res://dialogue/Nathan.dialogue"), "start")
+	if Input.is_action_just_pressed("Casius"):
+		DialogueManager.show_example_dialogue_balloon(load("res://dialogue/Casius.dialogue"), "start")
 		return
 	
 		
